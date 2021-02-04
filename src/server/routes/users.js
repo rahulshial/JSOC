ba0c228788b76575ab6ecfeb47744e60701e7e34
Rahul Shial <rahul.shial@gmail.com>
2021-02-04 16:42:11 -0700

    user route-1

--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const Router = express.Router();
-const sqlConnection = require('../lib/db.js');
-const sqlPool = require('./lib/db').pool;
+const sqlPool = require('../lib/db').pool;
 
-Router.get("/login", (req, res) => {
-  const queryString = `SELECT * FROM users;`;
-  sqlConnection.query(queryString, (err, row, fields) => {
+Router.get("/:email/:password", (req, res) => {
+  const email = req.params.email;
+  const password = req.params.password
+  const queryString = `
+  SELECT id FROM users
+  WHERE email=${email} 
+  AND password = ${password};`;
+
+  sqlPool.query(queryString, (err, row, fields) => {
     if (!err) {
       res.send(row)
     } else {
--- src/server/server.js
+++ src/server/server.js
@@ -2,20 +2,13 @@ const express = require('express');
 // const mysql = require('mysql');
 
 const app = express();
-const port = 8000;
-const table ='users';
+const cors = require('cors');
+const port = process.env.PORT || 3030;
+app.use(cors());
 
-const sqlPool = require('./lib/db').pool;
+const usersRoutes 	     	= require("./routes/users");
 
-app.get('/api/users', (req, res) => {
-  sqlPool.query(`select * from ${table}`, (err, rows) => {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(rows);
-    }
-  });
-});
+app.use("/users", usersRoutes);
 
 app.listen(port, () => {
   console.log(`App server now listening to port ${port}`);
