bbed5a65cf69036aace1a9360d268fb94215501a
Rahul Shial <rahul.shial@gmail.com>
2021-02-15 14:13:08 -0700

    login component refactored

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -16,9 +16,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Chip from "@material-ui/core/Chip";
 import FaceIcon from "@material-ui/icons/Face";
-import { TramRounded } from '@material-ui/icons';
 
 /** Local Imports */
+import useApplicationData from '../../hooks/useApplicationData';
 
 const Copyright = () => {
   return (
@@ -61,22 +61,23 @@ const useStyles = makeStyles((theme) => ({
 
 export function Login() {
   const classes = useStyles();
-  const [cookies, setCookie] = useCookies(["name"]);
-  const history = useHistory();
-  const [state, setState] = useState({
-    email:'',
-    password: '',
-    passwordConfirmation: '',
-    loginError: false,
-    loginErrorLabel: '',
-    mainButtonText: 'Sign In',
-    functionState: 'signIn',
-    secondButtonText: 'Forgot Password?',
-    thirdButtonText: "Don't have an account? Sign Up",
-    invalidPasswordError: false,
-    userExistsError: false,
-    errorBarColor: 'secondary',
-  });
+  const { state, setState, signInProcess, passwordResetProcess, signUpProcess } = useApplicationData();
+  // const [cookies, setCookie] = useCookies(["name"]);
+  // const history = useHistory();
+  // const [state, setState] = useState({
+  //   email:'',
+  //   password: '',
+  //   passwordConfirmation: '',
+  //   loginError: false,
+  //   loginErrorLabel: '',
+  //   mainButtonText: 'Sign In',
+  //   functionState: 'signIn',
+  //   secondButtonText: 'Forgot Password?',
+  //   thirdButtonText: "Don't have an account? Sign Up",
+  //   invalidPasswordError: false,
+  //   userExistsError: false,
+  //   errorBarColor: 'secondary',
+  // });
 
   const setEmail = (event) => {
     setState((prev) => ({
@@ -99,126 +100,126 @@ export function Login() {
     }));
   };
 
-  const signInProcess = () => {
-    const encodedPassword = encodeURIComponent(state.password);
-    axios
-    .get(`/users/${state.email}&${encodedPassword}`)
-    .then((res) => {
-      if (res.data.length > 0) {
-        if(res.data === 'server error') {
-          setState((prev) => ({
-            ...prev,
-            loginError: true,
-            loginErrorLabel: 'Server Error. Please try again later!!!',
-          }));
-        } else if (res.data === 'invalid user') {
-          setState((prev) => ({
-            ...prev,
-            loginError: true,
-            loginErrorLabel: 'Email / Password combination does not exist',
-          }));
-        } else {
-          const email = state.email;
-          const type = res.data[0].type;
-          setCookie("userLogged", { email, type }, { path: "/" });
-          history.push('/');
-          history.go(history.length - 1);
-          window.location.reload();
-        }
-      };
-    })
-    .catch((error) => {
-      console.log(error);
-      setState((prev) => ({
-        ...prev,
-        loginError: true,
-        loginErrorLabel: 'Server Error. Please try again later!!!',
-      }));
-    });
-  };
+  // const signInProcess = () => {
+  //   const encodedPassword = encodeURIComponent(state.password);
+  //   axios
+  //   .get(`/users/${state.email}&${encodedPassword}`)
+  //   .then((res) => {
+  //     if (res.data.length > 0) {
+  //       if(res.data === 'server error') {
+  //         setState((prev) => ({
+  //           ...prev,
+  //           loginError: true,
+  //           loginErrorLabel: 'Server Error. Please try again later!!!',
+  //         }));
+  //       } else if (res.data === 'invalid user') {
+  //         setState((prev) => ({
+  //           ...prev,
+  //           loginError: true,
+  //           loginErrorLabel: 'Email / Password combination does not exist',
+  //         }));
+  //       } else {
+  //         const email = state.email;
+  //         const type = res.data[0].type;
+  //         setCookie("userLogged", { email, type }, { path: "/" });
+  //         history.push('/');
+  //         history.go(history.length - 1);
+  //         window.location.reload();
+  //       }
+  //     };
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //     setState((prev) => ({
+  //       ...prev,
+  //       loginError: true,
+  //       loginErrorLabel: 'Server Error. Please try again later!!!',
+  //     }));
+  //   });
+  // };
 
-  const passwordResetProcess = () => {
-    axios
-    .post(`/users/password-reset/${state.email}`)
-    .then((res) => {
-      if (res.data.length > 0) {
-        if (res.data === 'invalid user') {
-          setState((prev) => ({
-            ...prev,
-            loginError: true,
-            loginErrorLabel: 'User email not found!',
-          }));
-        } else if (res.data === 'success') {
-          setState((prev) => ({
-            ...prev,
-            loginError: true,
-            loginErrorLabel: 'Please check your email for password reset email!',
-            errorBarColor: 'primary',
-            functionState: 'signIn',
-            mainButtonText: 'Sign In',
-            secondButtonText: 'Forgot Password?',
-            thirdButtonText: "Don't have an account? Sign Up",
-          }));
-        } else {
-          setState((prev) => ({
-            ...prev,
-            loginError: true,
-            loginErrorLabel: 'Error sending email, please try again later!',
-            functionState: 'signIn',
-            mainButtonText: 'Sign In',
-            secondButtonText: 'Forgot Password?',
-            thirdButtonText: "Don't have an account? Sign Up",
-          }));
-        }
-      }
-    })
-    .catch((error) => {
-      setState((prev) => ({
-        ...prev,
-        loginError: true,
-        loginErrorLabel: 'Error Retrieving Data, please try again later!',
-      }));
-    });
-  };
+  // const passwordResetProcess = () => {
+  //   axios
+  //   .post(`/users/password-reset/${state.email}`)
+  //   .then((res) => {
+  //     if (res.data.length > 0) {
+  //       if (res.data === 'invalid user') {
+  //         setState((prev) => ({
+  //           ...prev,
+  //           loginError: true,
+  //           loginErrorLabel: 'User email not found!',
+  //         }));
+  //       } else if (res.data === 'success') {
+  //         setState((prev) => ({
+  //           ...prev,
+  //           loginError: true,
+  //           loginErrorLabel: 'Please check your email for password reset email!',
+  //           errorBarColor: 'primary',
+  //           functionState: 'signIn',
+  //           mainButtonText: 'Sign In',
+  //           secondButtonText: 'Forgot Password?',
+  //           thirdButtonText: "Don't have an account? Sign Up",
+  //         }));
+  //       } else {
+  //         setState((prev) => ({
+  //           ...prev,
+  //           loginError: true,
+  //           loginErrorLabel: 'Error sending email, please try again later!',
+  //           functionState: 'signIn',
+  //           mainButtonText: 'Sign In',
+  //           secondButtonText: 'Forgot Password?',
+  //           thirdButtonText: "Don't have an account? Sign Up",
+  //         }));
+  //       }
+  //     }
+  //   })
+  //   .catch((error) => {
+  //     setState((prev) => ({
+  //       ...prev,
+  //       loginError: true,
+  //       loginErrorLabel: 'Error Retrieving Data, please try again later!',
+  //     }));
+  //   });
+  // };
 
-  const signUpProcess = () => {
-    if (state.password !== state.passwordConfirmation) {
-      setState((prev) => ({
-        ...prev,
-        loginError: true,
-        loginErrorLabel: 'Passwords do not match!',
-      }));
-    } else {
-      axios
-      .post(`/users/${state.email}&${state.password}`)
-      .then((res) => {
-        if(res.data.length > 0) {
-          if(res.data === "user exists") {
-            setState((prev) => ({
-              ...prev,
-              loginError: true,
-              loginErrorLabel: 'User Exists. Consider Sign In!!!',
-            }));  
-          } else if (res.data === 'success') {
-            const email = state.email;
-            const type = 'MEM';
-            setCookie("userLogged", { email, type }, { path: "/" });
-            history.push('/');
-            history.go(history.length - 1);
-            window.location.reload();    
-          }
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-        setState((prev) => ({
-          ...prev,
-          loginError: true,
-          loginErrorLabel: 'Server Error. Please try again later!!!',
-        }));
-      });
-    }
-  };
+  // const signUpProcess = () => {
+  //   if (state.password !== state.passwordConfirmation) {
+  //     setState((prev) => ({
+  //       ...prev,
+  //       loginError: true,
+  //       loginErrorLabel: 'Passwords do not match!',
+  //     }));
+  //   } else {
+  //     axios
+  //     .post(`/users/${state.email}&${state.password}`)
+  //     .then((res) => {
+  //       if(res.data.length > 0) {
+  //         if(res.data === "user exists") {
+  //           setState((prev) => ({
+  //             ...prev,
+  //             loginError: true,
+  //             loginErrorLabel: 'User Exists. Consider Sign In!!!',
+  //           }));  
+  //         } else if (res.data === 'success') {
+  //           const email = state.email;
+  //           const type = 'MEM';
+  //           setCookie("userLogged", { email, type }, { path: "/" });
+  //           history.push('/');
+  //           history.go(history.length - 1);
+  //           window.location.reload();    
+  //         }
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //       setState((prev) => ({
+  //         ...prev,
+  //         loginError: true,
+  //         loginErrorLabel: 'Server Error. Please try again later!!!',
+  //       }));
+  //     });
+  //   }
+  // };
 
   const processEvent = (event) => {
     event.preventDefault();
