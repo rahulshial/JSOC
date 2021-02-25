b0e1e614b1070220c0e45003bafed35c6c13edbd
Rahul Shial <rahul.shial@gmail.com>
2021-02-25 14:01:31 -0700

    database changes

--- DB/schema/jsoc_schema.sql
+++ DB/schema/jsoc_schema.sql
@@ -1,19 +1,19 @@
 -- -----------------------------------------------------
 -- Schema jsoc_db
 -- -----------------------------------------------------
-DROP DATABASE IF EXISTS `jsoc_db`;
-CREATE DATABASE IF NOT EXISTS `jsoc_db`;
-USE `jsoc_db` ;
+-- DROP DATABASE IF EXISTS `jsoc_db`;
+-- CREATE DATABASE IF NOT EXISTS `jsoc_db`;
+-- USE `jsoc_db` ;
 
 -- -----------------------------------------------------
 -- Drop all tables
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `users` CASCADE;
-DROP TABLE IF EXISTS 'news_announcements' CASCADE;
+DROP TABLE IF EXISTS `news_announcements` CASCADE;
 DROP TABLE IF EXISTS `activation` CASCADE;
 DROP TABLE IF EXISTS `events` CASCADE;
 DROP TABLE IF EXISTS `links` CASCADE;
-DROP TABLE IF EXISTS 'blog_posts' CASCADE;
+DROP TABLE IF EXISTS `blog_posts` CASCADE;
 
 -- -----------------------------------------------------
 -- Table `users`
@@ -23,6 +23,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` VARCHAR(255) NOT NULL,
   `password` VARCHAR(255) NULL DEFAULT NULL,
   `type` VARCHAR(255) NULL DEFAULT NULL,
+  `timeStamp` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);
 
@@ -34,6 +35,7 @@ CREATE TABLE IF NOT EXISTS `activation` (
   `email` VARCHAR(255) NOT NULL,
   `activation_token` VARCHAR(255) NULL DEFAULT NULL,
   `auth_token` VARCHAR(255) NULL DEFAULT NULL,
+  `timeStamp` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);
 
@@ -44,22 +46,24 @@ CREATE TABLE IF NOT EXISTS `news_announcements` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `type` TEXT NOT NULL,
   `message` VARCHAR(255) NOT NULL,
+  `timeStamp` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);
 
 -- -----------------------------------------------------
 -- Table `events`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `news_announcements` (
+CREATE TABLE IF NOT EXISTS `events` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(255) NOT NULL,
-  `description` VARCHAR(255) NOT NULL,,
+  `description` VARCHAR(255) NOT NULL,
   `venue` VARCHAR(255) NOT NULL,
   `start_date` DATE NOT NULL,
-  `send_date` DATE NOT NULL,
+  `end_date` DATE NOT NULL,
   `start_time` TIME NOT NULL,
-  `end _time` TIME NOT NULL,
-  `rsvp_required` BOOLEAN DEFAULT FALSE
+  `end_time` TIME NOT NULL,
+  `rsvp_required` BOOLEAN NOT NULL DEFAULT FALSE,
+  `timeStamp` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);
 
@@ -72,6 +76,7 @@ CREATE TABLE IF NOT EXISTS `links` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `type` TEXT NOT NULL,
   `url` VARCHAR(255) NOT NULL,
+  `timeStamp` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);
   
@@ -83,5 +88,6 @@ CREATE TABLE IF NOT EXISTS `blog_posts` (
   `author` VARCHAR(255) NOT NULL,
   `date` DATETIME NOT NULL,
   `content` VARCHAR(255) NOT NULL,
+  `timeStamp` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);
--- DB/seeds/jsoc_seed.sql
+++ DB/seeds/jsoc_seed.sql
@@ -12,3 +12,26 @@ INSERT INTO users (email, password, type) VALUES
 ('webcomm.jsoc@gmail.com', '1234', 'DIR'),
 ('pathshala@gmail.com', '1234', 'DIR'),
 ('rahul.shial@gmail.com', '1234', 'MEM');
+
+INSERT INTO news_announcements(type, message) VALUES
+('PRESMSG', "This is the President's blog area. You can type what ever message you want to pass to the community.");
+
+INSERT INTO events (title, description, venue, start_date, end_date, start_time, end_time) VALUES
+('Pathshala', 'Bi-Weekly Kids Pathshala', 'online', '2021-01-10','2021-01-10', '10:00:00', '11:30:00'),
+('Pathshala', 'Bi-Weekly Kids Pathshala', 'online', '2021-01-24','2021-01-24', '10:00:00', '11:30:00'),
+('Pathshala', 'Bi-Weekly Kids Pathshala', 'online', '2021-02-07','2021-02-07', '10:00:00', '11:30:00'),
+('Pathshala', 'Bi-Weekly Kids Pathshala', 'online', '2021-02-21','2021-02-21', '10:00:00', '11:30:00'),
+('Pathshala', 'Bi-Weekly Kids Pathshala', 'online', '2021-03-07','2021-03-07', '10:00:00', '11:30:00'),
+('Pathshala', 'Bi-Weekly Kids Pathshala', 'online', '2021-03-21','2021-03-21', '10:00:00', '11:30:00'),
+('Pathshala', 'Bi-Weekly Kids Pathshala', 'online', '2021-04-11','2021-04-11', '10:00:00', '11:30:00'),
+('Pathshala', 'Bi-Weekly Kids Pathshala', 'online', '2021-04-25','2021-04-25', '10:00:00', '11:30:00'),
+('Pathshala', 'Bi-Weekly Adults Pathshala', 'online', '2021-01-10','2021-01-10', '19:30:00', '21:00:00'),
+('Pathshala', 'Bi-Weekly Adults Pathshala', 'online', '2021-01-24','2021-01-24', '19:30:00', '21:00:00'),
+('Pathshala', 'Bi-Weekly Adults Pathshala', 'online', '2021-02-07','2021-02-07', '19:30:00', '21:00:00'),
+('Pathshala', 'Bi-Weekly Adults Pathshala', 'online', '2021-02-21','2021-02-21', '19:30:00', '21:00:00'),
+('Pathshala', 'Bi-Weekly Adults Pathshala', 'online', '2021-03-07','2021-03-07', '19:30:00', '21:00:00'),
+('Pathshala', 'Bi-Weekly Adults Pathshala', 'online', '2021-03-21','2021-03-21', '19:30:00', '21:00:00'),
+('Pathshala', 'Bi-Weekly Adults Pathshala', 'online', '2021-04-11','2021-04-11', '19:30:00', '21:00:00'),
+('Pathshala', 'Bi-Weekly Adults Pathshala', 'online', '2021-04-25','2021-04-25', '19:30:00', '21:00:00');
+
+
